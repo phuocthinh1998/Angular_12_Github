8361dc664204816fbdd06b781181347d95576312
Phuoc Thinh <phuocthinh11228899@gmail.com>
2021-09-13 17:26:45 +0700

    data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public name = "Truong Phuoc Thinh";
   constructor() { }
 
   ngOnInit(): void {
