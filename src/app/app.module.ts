9109600f1bf456a197378c9b68c17f263ceffcf0
Phuoc Thinh <phuocthinh11228899@gmail.com>
2021-09-14 17:37:33 +0700

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component'
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
