1cc019cc8dc4be6d839e0f872a09fad14e99d78b
Phuoc Thinh <phuocthinh11228899@gmail.com>
2021-09-14 17:30:56 +0700

    ngfor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   public name = "Truong Phuoc Thinh";
   public age = 18;
-  public traicay=['Sầu riêng','Xoài','Mít','Ổi']
+  public traicay=['Sầu riêng','Xoài','Mít','Ổi'];
+  public traicay2=[
+    {ten:'Xoài', gia:'100',km:true},
+    {ten:'Táo', gia:'200',km:true},
+    {ten:'Cóc', gia:'690',km:false},
+    {ten:'Chôm chôm', gia:'280',km:true}
+  ]
   constructor() { }
 
   ngOnInit(): void {
