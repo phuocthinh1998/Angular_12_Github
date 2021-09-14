b58145287f71ce8e6c6326e77379c8a820058249
Phuoc Thinh <phuocthinh11228899@gmail.com>
2021-09-14 09:55:30 +0700

    two way binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name = "Truong Phuoc Thinh";
-  public age = 18;
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  public resetName(): void{
+    console.log("Reset Name");
+    this.name = '';
+  }
 }
