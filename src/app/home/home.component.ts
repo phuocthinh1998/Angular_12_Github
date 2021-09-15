1a329ad47710a5e74817f2c37306bf477665591d
Phuoc Thinh <phuocthinh11228899@gmail.com>
2021-09-15 11:06:25 +0700

    combobox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,9 +15,44 @@ export class HomeComponent implements OnInit {
     {ten:'Cóc', gia:'690',km:false},
     {ten:'Chôm chôm', gia:'280',km:true}
   ]
+  public districts: string[]=['Quận/huyện'
+  ];
+  public cityData=[
+    {city:'Chọn thành phố',district:['Quận/huyện']},
+    {city: 'Cà Mau', district: [
+    'Thành phố Cà Mau',
+    'huyện Cái Nước',
+    'huyện Năm Căn',
+    'huyện Đầm Dơi',
+    'huyện U minh',
+    'huyện Phú Tân'
+  ],
+},
+{city: 'Bạc Liêu', district: [
+  'Thành phố Bạc Liêu',
+  'huyện Hồng Dân',
+  'huyện Phước Long',
+  'Thị xã Giá Rai',
+],
+}
+];
   constructor() { }
 
   ngOnInit(): void {
+    console.log('cities: ', this.cityData)
+  }
+  public changecity(event:any):void{
+    const city = event.target.value;
+    if(!city){
+      return;
+    }
+    // console.log('event',city);
+    // const search = this.cityData.filter((data)=>data.city===city)
+    // console.log('event',search);
+    // if(search && search.length>0){
+    //   this.districts=search[0].district;
+    // }
+    this.districts=this.cityData.find(data=>data.city===city)?.district || [];
   }
 
 }
